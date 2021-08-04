72ae8b1a6a87c82e86809524212628f380b299d2
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-08-04 17:25:43 +0200

    Create and insert dynammically a CTA card on illustration of main

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,20 @@
 const $header = document.querySelector("header");
+const $illustrationMain = document.querySelector("#illustration");
 
+// Set a banner on top of page
 const $celebrate = document.createElement("div");
 $celebrate.setAttribute("id", "celebrate");
 $celebrate.innerHTML = `<p>🎉 To celebrate Spence's launch, we will be waiving all fees for the entire month of April. <a href="#" title="Get offers">You will be receiving 100% of the earnings.</a> 🎉</p>`;
 $header.prepend($celebrate);
+
+// Set a call-to-action card on illustration of main
+
+const $ctaCard = document.createElement("div");
+$ctaCard.setAttribute("class", "card");
+$ctaCard.innerHTML =  `<p>April Earnings</p>
+                       <hr>
+                       <p><span class="bold">$272.21</span></p>
+                       <button type="button">Withdraw</button>
+                       `;
+$illustrationMain.appendChild($ctaCard);
+
